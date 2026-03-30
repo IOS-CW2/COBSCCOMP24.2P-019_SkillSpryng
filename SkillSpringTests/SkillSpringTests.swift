f8649a7d69fef528153eafa8b2ce17d7dfff98e3
ShaznaSalman <shaznasalman10@gmail.com>
2026-03-30 17:28:49 +0530

    Added test bundles

---
+++ SkillSpringTests/SkillSpringTests.swift
@@ -0,0 +1,16 @@
+//
+//  SkillSpringTests.swift
+//  SkillSpringTests
+//
+//  Created by COBSCCOMP24.2P-019 on 2026-03-30.
+//
+
+import Testing
+
+struct SkillSpringTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
