5478094dd745e76427cc8bc0908a7c1c7b876024
ShaznaSalman <shaznasalman10@gmail.com>
2026-03-30 17:16:45 +0530

    Initial Commit

---
+++ SkillSpring/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkillSpring
+//
+//  Created by COBSCCOMP24.2P-019 on 2026-03-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkillSpring/SkillSpringApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillSpringApp.swift
+//  SkillSpring
+//
+//  Created by COBSCCOMP24.2P-019 on 2026-03-30.
+//
+
+import SwiftUI
+
+@main
+struct SkillSpringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
